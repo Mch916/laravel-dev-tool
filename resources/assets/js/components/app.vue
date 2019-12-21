215e7f74c39ed39a75919445d3ca392fd60ad7ee
Lau Kwai Hung <kwaihung1314@laudeair.mynet>
2019-12-21 17:36:16 +0800

    init

---
+++ resources/assets/js/components/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-black w-1/2 h-5">
+  </div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      console.log('Component mounted.')
+    }
+  }
+</script>
